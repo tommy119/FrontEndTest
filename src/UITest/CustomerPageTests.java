9488ec5e6171f370c1a1b7a366fe5d60daed9da4
bossioaidan <61567246+bossioaidan@users.noreply.github.com>
2021-04-30 23:43:17 -0700

    Added single edit form verification test to CustomerPAgeTests.java

--- src/UITest/CustomerPageTests.java
+++ src/UITest/CustomerPageTests.java
@@ -28,13 +28,27 @@ public class CustomerPageTests {
 	}
 	
 	@Test(priority=0)
+	public void VerifySingelEditFormTest() throws InterruptedException {
+		driver.findElement(By.id("mat-checkbox-12")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("mat-button-toggle-1-button")).click();
+		Thread.sleep(3000);
+		
+		String editFormCode = driver.findElement(By.id("mat-input-0")).getAttribute("value");
+		String tableCode = driver.findElements(By.className("cdk-column-Code")).get(1).getText();
+		Thread.sleep(3000);
+
+		Assert.assertTrue(editFormCode.equals(tableCode));
+	}
+	
+	@Test(priority=1)
 	public void ViewTest() throws InterruptedException {
 		String pagLabel = driver.findElement(By.className("mat-paginator-range-label")).getText();
     	Assert.assertFalse(pagLabel.equals("0 of 0"));
     	Thread.sleep(3000);
 	}
 	
-	@Test(priority=1)
+	@Test(priority=2)
 	public void PaginationNextPageTest() throws InterruptedException {
 		driver.findElementByCssSelector("button.mat-paginator-navigation-next").click();
 		String pagLabel = driver.findElement(By.className("mat-paginator-range-label")).getText();
@@ -43,7 +57,7 @@ public class CustomerPageTests {
 		Assert.assertTrue(pagLabel.indexOf("21 – 40") != -1);
 	}
 	
-	@Test(priority=2)
+	@Test(priority=3)
 	public void PaginationPreviousPageTest() throws InterruptedException {
 		driver.findElementByCssSelector("button.mat-paginator-navigation-previous").click();
 		String pagLabel = driver.findElement(By.className("mat-paginator-range-label")).getText();
@@ -53,7 +67,7 @@ public class CustomerPageTests {
 	}
 	
 	
-	@Test(priority=3)
+	@Test(priority=4)
 	public void PaginationLastPageTest() throws InterruptedException {
 		driver.findElementByCssSelector("button.mat-paginator-navigation-last").click();
 		
@@ -67,19 +81,18 @@ public class CustomerPageTests {
 	}
 	
 	
-	@Test(priority=4)
+	@Test(priority=5)
 	public void PaginationFirstPageTest() throws InterruptedException {
 		driver.findElementByCssSelector("button.mat-paginator-navigation-first").click();
 		String pagLabel = driver.findElement(By.className("mat-paginator-range-label")).getText();
 		Thread.sleep(3000);
 		
 		Assert.assertTrue(pagLabel.indexOf("1 – 20") != -1);
-	}
-	
+	}	
 	
 	@AfterTest
 	public void end() {
-    	driver.close();
+    	//driver.close();
 	}
 
 }
