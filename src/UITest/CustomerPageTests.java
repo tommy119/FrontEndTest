417b371a440378eec5269a9027b17cc23c5bce2a
bossioaidan <61567246+bossioaidan@users.noreply.github.com>
2021-05-01 21:18:42 -0700

    Add, singleEdit, singleDelete, Multiedit(incomplete), multidelete(incomplete)

--- src/UITest/CustomerPageTests.java
+++ src/UITest/CustomerPageTests.java
@@ -22,25 +22,165 @@ public class CustomerPageTests {
     	Thread.sleep(1000);
     	ByAngularButtonText login = ByAngular.buttonText("LOG IN");
     	driver.findElement(login).click();
-    	Thread.sleep(3000);
+    	Thread.sleep(2000);
     	driver.get("http://dsd-office-web.s3-website-us-west-2.amazonaws.com/customers");
-    	Thread.sleep(3000);
+    	Thread.sleep(2000);
 	}
 	
 	@Test(priority=0)
+	public void VerifyAddTest() throws InterruptedException {
+		driver.findElement(By.id("mat-button-toggle-2-button")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("mat-input-0")).sendKeys("000001");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-1")).sendKeys("AmenRa");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-2")).sendKeys("Egypt");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-3")).sendKeys("Giza");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-8")).sendKeys("1");
+		Thread.sleep(1000);
+		
+    	driver.findElement(ByAngular.buttonText("Add Customer")).click();
+    	Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Confirm")).click();
+    	Thread.sleep(2000);
+		
+		String tableCode = driver.findElements(By.className("cdk-column-Code")).get(1).getText();
+		String tableName = driver.findElements(By.className("cdk-column-Name")).get(1).getText();
+		String tableAddress = driver.findElements(By.className("cdk-column-Address")).get(1).getText();
+		String tableCity = driver.findElements(By.className("cdk-column-City")).get(1).getText();
+		String tableStatus = driver.findElements(By.className("cdk-column-Active-Status")).get(1).getText();
+
+		Assert.assertTrue(tableCode.equals("000001"));
+		Assert.assertTrue(tableName.equals("AmenRa"));
+		Assert.assertTrue(tableAddress.equals("Egypt"));
+		Assert.assertTrue(tableCity.equals("Giza"));
+		Assert.assertTrue(tableStatus.equals("1"));
+	}	
+	
+	@Test(priority=1)
 	public void VerifySingelEditFormTest() throws InterruptedException {
-		driver.findElement(By.id("mat-checkbox-12")).click();
-		Thread.sleep(3000);
+		driver.findElement(By.id("mat-checkbox-32")).click();
+		Thread.sleep(1000);
 		driver.findElement(By.id("mat-button-toggle-1-button")).click();
-		Thread.sleep(3000);
+		Thread.sleep(1000);
 		
 		String editFormCode = driver.findElement(By.id("mat-input-0")).getAttribute("value");
 		String tableCode = driver.findElements(By.className("cdk-column-Code")).get(1).getText();
-		Thread.sleep(3000);
+		Thread.sleep(1000);
 
 		Assert.assertTrue(editFormCode.equals(tableCode));
+	}	
+	
+	@Test(priority=2)
+	public void VerifySingelEditTest() throws InterruptedException {
+		driver.findElement(By.id("mat-input-6")).clear();
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-6")).sendKeys("edited");
+		Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Save Changes")).click();
+    	Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Confirm")).click();
+    	Thread.sleep(2000);
+		String tableNote = driver.findElements(By.className("cdk-column-Note")).get(1).getText();
+		Thread.sleep(2000);
+
+		Assert.assertTrue(tableNote.equals("edited"));
 	}
 	
+	@Test(priority=3)
+	public void VerifySingelDeleteTest() throws InterruptedException {
+
+    	driver.findElement(ByAngular.buttonText("Delete Customer(s)")).click();
+    	Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Confirm")).click();
+    	Thread.sleep(2000);
+		String tableCode = driver.findElements(By.className("cdk-column-Code")).get(1).getText();
+		Thread.sleep(2000);
+
+		Assert.assertFalse(tableCode.equals("000001"));
+	}
+	
+	@Test(priority=4)
+	public void VerifyMultiEditTest() throws InterruptedException {
+		
+		driver.findElement(By.id("mat-button-toggle-2-button")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("mat-input-9")).sendKeys("000001");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-10")).sendKeys("AmenRa");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-11")).sendKeys("Egypt");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-12")).sendKeys("Giza");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-17")).sendKeys("1");
+		Thread.sleep(1000);
+		
+    	driver.findElement(ByAngular.buttonText("Add Customer")).click();
+    	Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Confirm")).click();
+    	Thread.sleep(2000);
+    	
+		driver.findElement(By.id("mat-input-0")).sendKeys("000002");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-1")).sendKeys("Anubis");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-2")).sendKeys("Duat");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-3")).sendKeys("Fourth Kingdom");
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-8")).sendKeys("1");
+		Thread.sleep(1000);
+		
+    	driver.findElement(ByAngular.buttonText("Add Customer")).click();
+    	Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Confirm")).click();
+    	Thread.sleep(2000);
+    	
+		driver.findElement(By.id("mat-checkbox-12")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-checkbox-13")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-button-toggle-1-button")).click();
+		Thread.sleep(1000);
+    	
+		driver.findElement(By.id("mat-input-6")).clear();
+		Thread.sleep(1000);
+		driver.findElement(By.id("mat-input-6")).sendKeys("multiedited");
+		Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Save Changes")).click();
+    	Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Confirm")).click();
+    	Thread.sleep(2000);
+		String tableNote1 = driver.findElements(By.className("cdk-column-Note")).get(1).getText();
+		String tableNote2 = driver.findElements(By.className("cdk-column-Note")).get(2).getText();
+		Thread.sleep(2000);
+		
+		Assert.assertTrue(tableNote1.equals("multiedited"));
+		Assert.assertTrue(tableNote2.equals("multiedited"));
+	}
+	
+	@Test(priority=5)
+	public void VerifyMultiDeleteTest() throws InterruptedException {
+
+    	driver.findElement(ByAngular.buttonText("Delete Customer(s)")).click();
+    	Thread.sleep(2000);
+    	driver.findElement(ByAngular.buttonText("Confirm")).click();
+    	Thread.sleep(2000);
+		String tableCode1 = driver.findElements(By.className("cdk-column-Code")).get(1).getText();
+		String tableCode2 = driver.findElements(By.className("cdk-column-Code")).get(2).getText();
+		Thread.sleep(2000);
+
+		Assert.assertFalse(tableCode1.equals("000001"));
+		Assert.assertFalse(tableCode2.equals("000002"));
+	}
+	
+	/*
 	@Test(priority=1)
 	public void ViewTest() throws InterruptedException {
 		String pagLabel = driver.findElement(By.className("mat-paginator-range-label")).getText();
@@ -88,7 +228,7 @@ public class CustomerPageTests {
 		Thread.sleep(3000);
 		
 		Assert.assertTrue(pagLabel.indexOf("1 – 20") != -1);
-	}	
+	}	*/
 	
 	@AfterTest
 	public void end() {
